741e3217493be7859b6dcee07d8816cd898454da
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-03-18 12:18:29 +0200

    Created model for reservation using Sequelize

---
+++ backend/models/Reservation.js
@@ -0,0 +1,41 @@
+import { DataTypes, Model } from 'sequelize';
+
+/**
+ * Represents a reservation.
+ * @class
+ */
+class Reservation extends Model {}
+
+Reservation.init(
+  {
+    reservation_number: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      unique: true,
+    },
+    customer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    customer_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        max: {
+          args: 8,
+          msg: 'For reservations larger than 8 people please call us!',
+        },
+      },
+    },
+    reservation_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: any, // Placeholder
+    modelName: 'Reservation',
+    timestamps: true,
+  }
+);
