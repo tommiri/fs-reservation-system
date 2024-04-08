99406551c71634b9584f17cc792fa22eef5b8906
KayTee1 <truong.kha@outlook.com>
2024-04-08 11:49:39 +0300

    style navbar add logo.png add hover animation to navItems

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,28 +1,46 @@
 import React from "react";
-import NavItem from "./NavItem";
 import styled from "styled-components";
+
+import NavItem from "./NavItem";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.nav`
   display: flex;
-  padding: 1rem 0;
-  width: 100%;
-  background-color: #4CAF50;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #272727;
 `;
 
 const NavContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  gap: 20px;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+  width: 321px;
+  max-height: 61px;
+`;
+
+const LogoContainer = styled.div`
+  cursor: pointer;
 `;
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <Container>
+      <LogoContainer
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <Image src="./public/logo.png" alt="logo" />
+      </LogoContainer>
       <NavContainer>
-        <NavItem name="Home" to="/" />
-        <NavItem name="Reservation" to="/reservation" />
-        <NavItem name="Manage" to="/manage" />
+        <NavItem name="Book a Table" to="/reservation" />
+        <NavItem name="View your Reservation" to="/manage" />
       </NavContainer>
     </Container>
   );
--- frontend/src/components/NavItem.jsx
+++ frontend/src/components/NavItem.jsx
@@ -4,15 +4,36 @@ import { NavLink } from "react-router-dom";
 
 const Container = styled.div`
   margin: 0 10px;
-  background-color: white;
   border-radius: 5px;
   padding: 0.5rem 1rem;
 `;
 const StyledNavLink = styled(NavLink)`
-  color: black;
-  text-decoration: none;
+  color: white;
+  font-size: 1.5rem;
   padding: 0.5rem 1rem;
   border-radius: 5px;
+  position: relative;
+  text-decoration: none;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 2px;
+    background-color: gray; /* Change to your desired color */
+    transition: width 0.3s ease; /* Adjust the transition duration as needed */
+  }
+
+  &:hover {
+    color: lightgray; /* Change to your desired hover color */
+  }
+
+  &:hover::before {
+    width: 100%;
+  }
 `;
 
 const NavItem = ({ name, to }) => {
