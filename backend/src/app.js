f072e8a6da6af57baee9eec00ea121c6d7cfa03e
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-04-08 14:53:33 +0300

    Added routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const reservations = require('./routes/reservations');
+
 const app = express();
 app.use(express.json());
+
+app.use('/reservations', reservations);
 
 app.get('/health', (_req, res) => {
   res.send('OK');
