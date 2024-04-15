4e13ffef963a43f15ff95ea8dddaf946b7061d4f
Jone5s <axu.lepp@gmail.com>
2024-04-15 13:49:29 +0300

    Added reservation adding functionality, also updated cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const reservations = require('./routes/reservations');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ]
+}));
 
 app.use('/reservations', reservations);
 
