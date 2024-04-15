4b2354fc48ca2bccd23adeb8f5ae93adcd60b50a
hennasavilahti <savilahtihenna@gmail.com>
2024-04-15 12:20:33 +0300

    Finishing touch to pages

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -1,14 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 0 10px;
-  padding: 0.5rem 1rem;
+  padding: 3rem 1rem;
+  text-align: center;
+  margin-top: 60px;
 `;
 export default function FrontPage() {
   return (
     <Container>
       <h1>Welcome to ...</h1>
-      <p>
+
+      <p style={{ marginTop: '2rem' }}>
         This is the front page of the reservation system. Use the navigation bar
         to navigate to the reservation page or the management page.
       </p>
