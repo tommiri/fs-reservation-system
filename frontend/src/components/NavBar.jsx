7478a02e43705450ce8601d2c198def1ec43c179
hennasavilahti <savilahtihenna@gmail.com>
2024-04-15 12:19:42 +0300

    Made NavBar fixed and more centered

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import NavItem from "./NavItem";
-import { useNavigate } from "react-router-dom";
+import NavItem from './NavItem';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.nav`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   background-color: #272727;
+  box-shadow: 0 0 10px 0 #00000060;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 60px;
+  align-items: center;
 `;
 
 const NavContainer = styled.div`
@@ -21,6 +28,7 @@ const Image = styled.img`
   height: auto;
   width: 321px;
   max-height: 61px;
+  margin-top: 4px;
 `;
 
 const LogoContainer = styled.div`
@@ -33,7 +41,7 @@ const NavBar = () => {
     <Container>
       <LogoContainer
         onClick={() => {
-          navigate("/");
+          navigate('/');
         }}
       >
         <Image src="./public/logo.png" alt="logo" />
