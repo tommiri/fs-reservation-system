91256c85e844fe1fbda672885a265c2b29359e7d
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-04-22 17:48:01 +0300

    Fixed confirmation email sending

--- backend/src/utility/mailer.js
+++ backend/src/utility/mailer.js
@@ -1,19 +1,28 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const sendReservationEmail = async (reservationDetails) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
+      pass: process.env.EMAIL_APP_PASSWORD,
+    },
   });
+
+  // Format ISO time to Finnish time
+  const formattedDateTime = new Date(
+    reservationDetails.reservation_datetime
+  ).toLocaleString('en-FI');
 
   const mailOptions = {
     from: process.env.EMAIL_USERNAME,
-    to: reservationDetails.customerEmail,
+    to: reservationDetails.customer_email,
     subject: 'Reservation Confirmation',
-    text: `Hello ${reservationDetails.customerName},\n\nYour reservation for ${reservationDetails.customerCount} guests is confirmed on ${reservationDetails.reservationDatetime}.\nYour reservation number is ${reservationDetails.reservationNumber}.\n\nThank you for choosing us!`
+    text: `Hello ${reservationDetails.customer_name},\n\nYour reservation for ${reservationDetails.customer_count} guests is confirmed on ${formattedDateTime}.\nYour reservation number is ${reservationDetails.reservation_number}.\n\nThank you for choosing us!`,
+    html: `<h1>Your reservation at SomeCoolDiner</h1> <p>Hello ${reservationDetails.customer_name},</p><p>Your reservation for ${reservationDetails.customer_count} guests is confirmed on ${formattedDateTime}.</p><p>Your reservation number is ${reservationDetails.reservation_number}.</p><p>Thank you for choosing us!</p>`,
   };
 
   try {
