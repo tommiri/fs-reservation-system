88b02962d80b6da8b1c5ca0891a353e4b95e786c
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-04-22 20:06:02 +0300

    Fixed typo in host: DB_HOST --> MYSQL_HOST - host was undefined because of the type

--- backend/src/db/sequelize.js
+++ backend/src/db/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
+  host: process.env.MYSQL_HOST,
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
