1ddc3242e0043e775c197e7a9bb86f0977aca2a4
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-04-25 14:13:13 +0300

    Added restaurant name to frontpage

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -9,11 +9,12 @@ const Container = styled.div`
 export default function FrontPage() {
   return (
     <Container>
-      <h1>Welcome to ...</h1>
+      <h1>Welcome to SomeCoolDiner</h1>
 
       <p style={{ marginTop: '2rem' }}>
-        This is the front page of the reservation system. Use the navigation bar
-        to navigate to the reservation page or the management page.
+        This is the front page of the reservation system. Use the
+        navigation bar to navigate to the reservation page or the
+        management page.
       </p>
     </Container>
   );
